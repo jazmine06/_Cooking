da23e278cbd2baae9953f962faa7186781bcb20e
jazmine06 <jasminezulcha@gmail.com>
2022-12-07 12:41:17 +0700

    first commit

---
+++ Cooking/lib/data/resep.dart
@@ -0,0 +1,36 @@
+class resep {
+  String name, htm, tutorial, image;
+
+  resep(
+      {required this.name,
+      required this.htm,
+      required this.tutorial,
+      required this.image});
+}
+
+List<resep> dataResep = [
+  resep(
+      name: 'kopel keluarga',
+      htm: '45K',
+      tutorial:
+          'ini baju kopel untuk keluarga saat lebaran. bisa di pakai usia muda hingga tua dan semua kalangan dari yang mampu dan kurang mampu. alhamdulillah terpercaya',
+      image: 'model1.jpg'),
+  resep(
+      name: 'kopel keluarga',
+      htm: '45K',
+      tutorial:
+          'ini baju kopel untuk keluarga saat lebaran. bisa di pakai usia muda hingga tua dan semua kalangan dari yang mampu dan kurang mampu. alhamdulillah terpercaya',
+      image: 'model1.jpg'),
+  resep(
+      name: 'baju cewek muslim',
+      htm: '60K',
+      tutorial:
+          'halo ini produk yang bagus banget lohhh kita bisa beli dengan harga murah dan bagus banget hahahahah',
+      image: 'produk22.jpg'),
+  resep(
+      name: 'baju cewek muslim',
+      htm: '60K',
+      tutorial:
+          'halo ini produk yang bagus banget lohhh kita bisa beli dengan harga murah dan bagus banget hahahahah',
+      image: 'produk22.jpg'),
+];
